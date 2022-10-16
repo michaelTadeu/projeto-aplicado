554bd1e5ade58dc20093493d57b2a38495b4e53b
michaelTadeu <michael89.oliveira@gmail.com>
2022-10-15 22:42:21 -0300

    Commit Etapa 2 do projeto aplicado.

---
+++ Fonte/backend/insidenutri/src/main/java/com/insidenutri/insidenutri/InsidenutriApplication.java
@@ -0,0 +1,13 @@
+package com.insidenutri.insidenutri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsidenutriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsidenutriApplication.class, args);
+	}
+
+}
